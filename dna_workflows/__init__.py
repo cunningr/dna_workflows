9004863c3776425b433e154ddded9bc0c7e65025
Richard Cunningham <cunningr@cisco.com>
2020-08-23 23:05:15 +0100

    adding coloredlogs version

--- dna_workflows/__init__.py
+++ dna_workflows/__init__.py
@@ -6,7 +6,7 @@ DNA Workflows workflow engine module
 
 """
 
-__version__ = "0.0.15"
+__version__ = "0.0.16"
 
 from .wf_engine import *
 from .wf_client import *
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   package_data={
     'dna_workflows': ['module']
   },
-  version='0.0.15',
+  version='0.0.16',
   license='MIT',
   description='dna_workflows is a basic workflow engine for executing DNA Workflows packages',
   author='Richard Cunningham',
